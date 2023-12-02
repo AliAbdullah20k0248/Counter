8bd6247caa8b5afcbd885377944491dbc167407b
k200249ALiAbdullah <k200248@nu.edu.pk>
2023-12-03 00:03:05 +0500

    added he

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Counter from "./Counter";
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>he</h1>
       <Counter initialCount={0} />
     </div>
   );
